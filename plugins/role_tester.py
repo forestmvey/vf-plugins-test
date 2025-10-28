7f7816c7e5413d09f06c52c05c617e3ef7ce420d
forestmvey <forestv@bitquilltech.com>
2025-10-28 12:09:57 -0700

    cleanup
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/role_tester.py
+++ plugins/role_tester.py
@@ -1,16 +1,6 @@
 import boto3
-
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    # Create a session (uses your default profile or environment variables)
     session = boto3.Session()
-
-    # Get the credentials object
     credentials = session.get_credentials()
-
-    # Resolve the credentials (in case they're lazy-loaded)
     creds = credentials.get_frozen_credentials()
-
-    # Print them out
-    influxdb3_local.info(f"Caller identity ARN: " + creds.access_key)
-
-
+    influxdb3_local.info("Caller identity ARN: " + creds.access_key)
