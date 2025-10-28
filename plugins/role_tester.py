d46c9cbc745974176c90f9fd914cfc19455f3c98
forestmvey <forestv@bitquilltech.com>
2025-10-28 12:04:03 -0700

    Quick test
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/role_tester.py
+++ plugins/role_tester.py
@@ -1,17 +1,16 @@
-import botocore.session
-import requests
+import boto3
 
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    session = botocore.session.get_session()
-    credentials = session.get_credentials().get_frozen_credentials()
-    sts = session.create_client('sts')
-    identity = sts.get_caller_identity()
-    arn = identity['Arn']
-    influxdb3_local.info(f"Caller identity ARN: " + arn)
-    if ":assumed-role/" in arn:
-        role_name = arn.split(":assumed-role/")[1].split("/")[0]
-        influxdb3_local.info(f"Role name: " + role_name)
-    else:
-        ifluxdb3_local.info("Not using an assumed role (may be a user or root identity).")
+    # Create a session (uses your default profile or environment variables)
+    session = boto3.Session()
+
+    # Get the credentials object
+    credentials = session.get_credentials()
+
+    # Resolve the credentials (in case they're lazy-loaded)
+    creds = credentials.get_frozen_credentials()
+
+    # Print them out
+    influxdb3_local.info(f"Caller identity ARN: " + creds.access_key)
 
 
