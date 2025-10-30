0db00b009cf6c9e48f19e0d183323dcea3d6060a
forestmvey <forestv@bitquilltech.com>
2025-10-30 08:50:13 -0700

    Testing internet connection
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/test_loc.py
+++ plugins/test_loc.py
@@ -1,6 +1,10 @@
-import os
+import requests
 
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    influxdb3_local.info("HTTP_PROXY:", os.environ.get("HTTP_PROXY"))
-    influxdb3_local.info("NO_PROXY:", os.environ.get("NO_PROXY"))
-
+    url = f"http://example.com"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()  # Raise error for HTTP errors
+        influxdb3_local.info(response.text)         # Print the JSON credentials
+    except requests.RequestException as e:
+        influxdb3_local.info(f"Error fetching credentials: {e}")
