61e8165ffb0254586bd92f9098f9ba2b945a85dc
forestmvey <forestv@bitquilltech.com>
2025-11-07 13:54:59 -0800

    Alternate http approach
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/role_tester.py
+++ plugins/role_tester.py
@@ -1,16 +1,15 @@
-import socket
+import urllib.request
+import os
+import json
 
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    host = "169.254.170.2"
-    port = 80
+    url = f"http://169.254.170.2{os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']}"
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(10)
-    try:
-        s.connect((host, port))
-        influxdb3_local.info("Socket connection successful")
-    except Exception as e:
-        influxdb3_local.info(f"Socket connection failed: {e}")
-    finally:
-        s.close()
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'python-urllib/3.7')]
+    # Disable proxying (important in ECS/Fargate sometimes)
+    opener.add_handler(urllib.request.ProxyHandler({}))
+
+    with opener.open(url, timeout=10) as response:
+        influxdb3_local.info(json.loads(response.read()))
 
