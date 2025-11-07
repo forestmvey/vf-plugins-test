0c7a3ea584dc9f43429700fcac9c8bc234c8333d
forestmvey <forestv@bitquilltech.com>
2025-11-06 16:31:54 -0800

    Updating to module handling.
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/role_tester.py
+++ plugins/role_tester.py
@@ -1,6 +1,6 @@
 import boto3
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    session = boto3.Session()
+    session = boto3.session.Session()
     credentials = session.get_credentials()
     creds = credentials.get_frozen_credentials()
     influxdb3_local.info("Caller identity ARN: " + creds.access_key)
