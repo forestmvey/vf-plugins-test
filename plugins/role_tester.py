b4de58cd7f9192c4fb30cdb9f11f0b79ed47bc98
forestmvey <forestv@bitquilltech.com>
2025-11-07 14:13:07 -0800

    proxy test
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- plugins/role_tester.py
+++ plugins/role_tester.py
@@ -1,14 +1,4 @@
-import http.client
-http.client.HTTPConnection.debuglevel = 1
-
 import urllib.request
-import os
-
 def process_scheduled_call(influxdb3_local, call_time, args=None):
-    url = f"http://169.254.170.2{os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']}"
-    opener = urllib.request.build_opener(urllib.request.ProxyHandler({}))
-    urllib.request.install_opener(opener)
-
-    with urllib.request.urlopen(url, timeout=10) as r:
-        influxdb3_local.info(r.read())
+    influxdb3_local.info("Detected proxies:", urllib.request.getproxies())
 
